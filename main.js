6fa5bcde5696d6aa6ad91c384b45463a1d12facd
SwarnimMayukh <88070589+SwarnimMayukh@users.noreply.github.com>
2021-07-29 10:59:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function Delhi() {
+    window.alert("There are many places to visit in Delhi but Red Forte, Qutub Minar and Humayun's Tomb")
+}
+
+function TajMahal() {
+    window.alert("Taj Mahal is one of the 7 wonders of world")
+}
+
+function Way() {
+    window.alert("If your planning to go to Mumbai you must have to visit the Gate Way Of India. It's one of the famous places in India.")
+}
+
+function Rajasthan() {
+    window.alert("You must have to visit Gadi Sagar Temple in  Jaisalmer, Rajasthan . It's one of the famous places in Rajasthan and in India too.");
+}
+function Rishikesh() {
+     window.alert("Riskesh has so many places to visit but I cannot mention all of them like Jumpin Heights - Bungee Jumping,Neelkanth Mahadev, TempleRam Jhula,Parmarth Niketan and Kaudiyala is a small village and so on. You need to go to Riskesh by yourself to visit all these intresting places :).")
+}
+function Varanasi() {
+    window.alert("Varanasi is a beutiful city. Do you know Varanasi is one of the oldest city in India. there so many gahats to visit. Varanasi is on the bank of the holy river Ganga .")
+}
+function Amritsar() {
+    window.alert("Amritsar is a beutiful city. Do you know that Amritsar has the worlds largest kitchen which can server 100,000 diners (including curious tourists!) lentils and curries every day.Amritsar also has the fabulous Golden Temple. You must have to visit Golden Temple. ")
+}
